07a4cdbc2108fc2a3a532bff6bff114c6ea2e20c
99pmore <morenomartin.pablo@gmail.com>
2023-01-16 17:01:50 +0100

    firebase connection

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { List } from './List'
 import { Note } from './Note'
+
+import { db } from "../config/firebase";
+import { set, ref, push } from "firebase/database";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList } from "@fortawesome/free-solid-svg-icons";
 import { faNoteSticky } from "@fortawesome/free-regular-svg-icons";
@@ -8,29 +12,39 @@ import { faNoteSticky } from "@fortawesome/free-regular-svg-icons";
 export const Form = () => {
 
     const [currentComponent, setCurrentComponent] = useState()
+    const [note, setNote] = useState({ title: '', body: '' })
 
     const handleClickList = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         setCurrentComponent(<List />)
     }
 
     const handleClickNote = (e) => {
-        e.preventDefault();
-        setCurrentComponent(<Note />)
+        e.preventDefault()
+        setCurrentComponent(<Note note = { note } setNote = { setNote } />)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        push(ref(db, 'notes/'), {
+            title: note.title,
+            body: note.body
+        })
     }
 
     return (
-        <form>
+        <form onSubmit={ handleSubmit }>
             <div className='head'>
-                <input type='text' placeholder='Añade un título' />
+                <input type='text' placeholder='Añade un título' onChange={ e => setNote({ ...note, title: e.target.value }) } />
                 <button onClick={ handleClickList }><FontAwesomeIcon icon={ faList } /></button>
                 <button onClick={ handleClickNote }><FontAwesomeIcon icon={ faNoteSticky } /></button>
             </div>
 
             <div className='body'>
                 { currentComponent }
-                <button className='add-button'>Añadir</button>
             </div>
+
+            <button type='submit' className='add-button'>Añadir</button>
         </form>
     )
 }
