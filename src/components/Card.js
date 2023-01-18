ecf79632621585065cd3511329869ea5989861fe
99pmore <morenomartin.pablo@gmail.com>
2023-01-18 19:44:40 +0100

    checkbox added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 export const Card = ({ title, body, items }) => {
 
+    const [checkedItems, setCheckedItems] = useState([])
+
+    const handleCheckboxChange = (index) => {
+        if (checkedItems.includes(index)) {
+            setCheckedItems(checkedItems.filter((i) => i !== index))
+
+        } else {
+            setCheckedItems([ ...checkedItems, index ])
+        }
+    }
+
     const listItems = items ? items.map((item, index) => (
-        <li key={index}>{item}</li>
+        <li>
+            <input type="checkbox" className={`${checkedItems.includes(index) ? 'is-checked' : ''}`} checked={ checkedItems.includes(index) } onChange={ () => handleCheckboxChange(index) } />
+            <p key={index} className={checkedItems.includes(index) ? "strike" : ""}> { item }</p>
+        </li>
     )) : null
 
     return (
         <div className="card">
             <h3 className="title">{ title }</h3>
-            <p className="note-body">
-                { body }
-            </p>
-            <ul className="list-items">
-                { listItems }
-            </ul>
+            <p className="note-body">{ body }</p>
+            <ul className="list-items">{ listItems }</ul>
         </div>
     )
 }
