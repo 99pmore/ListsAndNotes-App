c8291056d43ebbcff13f64b919d0538552651e5e
99pmore <morenomartin.pablo@gmail.com>
2023-01-30 13:28:59 +0100

    checkbox update method for firebase

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,31 @@
 import { useState } from "react";
 
 import { db } from "../config/firebase";
-import { remove, ref } from "firebase/database";
+import { remove, update, ref } from "firebase/database";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import Swal from "sweetalert2";
 
 export const Card = ({ listId, noteId, title, body, items, user }) => {
-    const [checkedItems, setCheckedItems] = useState([]);
 
-    const handleCheckboxChange = (index) => {
+    const [checkedItems, setCheckedItems] = useState(
+        items
+          ? items.map((item, index) => (item.checkbox ? index : null))
+          .filter((i) => i !== null)
+        : []
+      )
+
+    const handleCheckboxChange = async (index) => {
         if (checkedItems.includes(index)) {
-            setCheckedItems(checkedItems.filter((i) => i !== index));
+            setCheckedItems(checkedItems.filter((i) => i !== index))
+
         } else {
-            setCheckedItems([...checkedItems, index]);
+            setCheckedItems([...checkedItems, index])
         }
+
+        const dbRef = ref(db, `users/${user.uid}/info/lists/${listId}/items/${index}`)
+        await update(dbRef, { checkbox: !items[index].checkbox })
     }
 
     const deleteNote = () => {
@@ -28,49 +38,43 @@ export const Card = ({ listId, noteId, title, body, items, user }) => {
             cancelButtonColor: "#29323c",
             confirmButtonText: "Eliminar",
             cancelButtonText: "Cancelar"
+
         }).then((result) => {
             if (result.isConfirmed) {
-                Swal.fire("Eliminado", "La nota se ha eliminado", "success");
+                Swal.fire("Eliminado", "La nota se ha eliminado", "success")
+
                 if (items) {
-                    remove(ref(db, `users/${user.uid}/info/lists/${listId}`));
+                    remove(ref(db, `users/${user.uid}/info/lists/${listId}`))
+
                 } else {
-                    remove(ref(db, `users/${user.uid}/info/notes/${noteId}`));
+                    remove(ref(db, `users/${user.uid}/info/notes/${noteId}`))
                 }
             }
         })
     }
 
-    const listItems = items
-        ? items.map((item, index) => (
+    const listItems = items ? items.map((item, index) => (
               <li key={`${listId}-${index}`}>
                   <input
-                      type="checkbox"
-                      className={`${
-                          checkedItems.includes(index) ? "is-checked" : ""
-                      }`}
-                      checked={checkedItems.includes(index)}
-                      onChange={() => handleCheckboxChange(index)}
+                        type="checkbox"
+                        className={ `${ checkedItems.includes(index) ? "is-checked" : "" }` }
+                        checked={ checkedItems.includes(index) }
+                        onChange={ () => handleCheckboxChange(index) }
                   />
-                  <p
-                      key={index}
-                      className={checkedItems.includes(index) ? "strike" : ""}
-                  >
-                      {" "}
-                      {item}
-                  </p>
+                  <p key={ index } className={ checkedItems.includes(index) ? "strike" : "" }> { item.text } </p>
               </li>
           ))
-        : null;
+        : null
 
     return (
         <div className="card">
-            <h3 className="title">{title}</h3>
-            <p className="note-body">{body}</p>
-            <ul className="list-items">{listItems}</ul>
+            <h3 className="title">{ title }</h3>
+            <p className="note-body">{ body }</p>
+            <ul className="list-items">{ listItems }</ul>
 
-            <div className="trash" onClick={deleteNote}>
-                <FontAwesomeIcon icon={faTrashCan} />
+            <div className="trash" onClick={ deleteNote }>
+                <FontAwesomeIcon icon={ faTrashCan } />
             </div>
         </div>
-    );
-};
+    )
+}
