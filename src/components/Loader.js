c4484fbb991ee0ef1f50c920ba86fc7af42a5b22
99pmore <morenomartin.pablo@gmail.com>
2023-02-07 12:38:00 +0100

    Loader component created

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+export const Loader = () => {
+
+    return (
+        <div className="wrapper">
+            <div class="loader">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="shadow"></div>
+                <div class="shadow"></div>
+                <div class="shadow"></div>
+            </div>
+        </div>
+    )
+}
